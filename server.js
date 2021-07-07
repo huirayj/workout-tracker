ce0d5fd3f282cc5c143a848b8976a17adbfa6d90
Justin Hui <huirayj@gmail.com>
2021-07-07 15:59:56 -0400

    added connection to mongoose

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const routes = require('./routes/api');
+const routes = require('./routes');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 app.use(routes);
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
 
 app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`));
